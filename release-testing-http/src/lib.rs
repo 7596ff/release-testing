5ae9075e6542013bb0b5093f40793ae81c54ba51
Cassandra McCarthy <cassie@7596ff.com>
2022-08-08 16:00:57 -0400

    feat(http): add method to get a channel (#3)
    Fixes #2.

--- release-testing-http/src/lib.rs
+++ release-testing-http/src/lib.rs
@@ -1,8 +1,14 @@
-use release_testing_model::Message;
+use release_testing_model::{Channel, Message};
 
 pub fn get_message() -> Message {
     Message {
         content: String::from("message content"),
+    }
+}
+
+pub fn get_channel() -> Message {
+    Channel {
+        name: String::from("channel name"),
     }
 }
 
